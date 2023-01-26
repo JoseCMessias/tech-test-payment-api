62a518dc9224e5b24e06856051b9ef78e1d9d057
José Messias <104660308+JoseCMessias@users.noreply.github.com>
2023-01-26 09:54:36 -0300

    Update Venda.cs

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -7,8 +7,6 @@ namespace tech_test_payment_api.Models
 {
     public class Venda : Vendedor
     {
-        // Uma venda contém informação sobre o vendedor que a efetivou, data, identificador do pedido e os itens que foram vendidos;
-
         public int IdDoPedido { get; set; }
         public int IdDoItem { get; set; }
         public DateTime Data { get; set; }
